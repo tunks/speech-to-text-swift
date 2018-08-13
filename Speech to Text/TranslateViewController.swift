adfa92f6667bb639d600af1b8123471a1111daf6
ebrimatunkara <ebrimatunkara@gmail.com>
2018-08-13 09:03:21 -0400

    add translator view

---
+++ Speech to Text/TranslateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateViewController.swift
+//  Speech to Text
+//
+//  Created by Ebrima Tunkara on 8/13/18.
+//  Copyright © 2018 IBM Watson Developer Cloud. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TranslateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         print("Translate view controller view loaded!")
+    }
+}
